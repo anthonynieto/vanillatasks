c743732800b734439a6ffc5d9d0d857e08a3ab4c
Anthony Nieto <sfnieto@gmail.com>
2020-10-23 18:39:09 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,42 +1,15 @@
-// Create variables for Submit & Clear buttons for Event Listeners
+// Variables for Submit & Clear buttons for Event Listeners
 const submitbutton = document.getElementById("submit");
 const clearbutton = document.getElementById("clearbutton");
 const taskinput = document.getElementById("enter_task");
 const ul = document.querySelector("ul");
 
-// tasklist = JSON.parse(window.localStorage.getItem('tasks'));
+// Retrieve Local Storage Task Items
+getTasks();
 
-// DOM Event Listener
-var DOMContent = document.addEventListener('DOMContentLoaded', getTasks);
-
-// Submit Button adds to Local Storage and DOM
-submitbutton.addEventListener("click", function(){
-	if (taskinput.value.length < 4){
-		alert("ERROR: please be more specific");
-		taskinput.value = "";
-	}
-
-	else if (taskinput.value.length > 20) {
-		alert ("ERROR: please be more brief");
-		taskinput.value = "";
-	}	
-		else {
-			// Update the DOM
-			const li = document.createElement("li");
-			ul.appendChild(li);
-			li.appendChild(document.createTextNode(taskinput.value));
-
-			// Store in LocalStorage
-			storeInLocalStorage(taskinput.value);
-
-			// Clear Task
-			taskinput.value = "";
-		}
-});
-
-// Get Tasks from LS
+// Function to Retrieve from Local Storage
 function getTasks(){
-	let tasks;
+let tasks;
 	if (localStorage.getItem('tasks') === null){
 		tasks = [];
 	} else {
@@ -45,12 +18,14 @@ function getTasks(){
 
 	tasks.forEach(function(task){
 	
+		// DOM Construction for each task
 		const li = document.createElement("li");
 		ul.appendChild(li);
 		li.appendChild(document.createTextNode(task));
 	})
 };
 
+// Function to Store in Local Storage
 function storeInLocalStorage(task){
 	let tasks;
 	if (localStorage.getItem('tasks') === null){
@@ -61,11 +36,36 @@ function storeInLocalStorage(task){
 	}
 
 	tasks.push(task);
-
 	localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
-// Clear Tasks AND local storage
+// Submit Button Event Listener, Local Storage, and DOM
+submitbutton.addEventListener("click", function(){
+	if (taskinput.value.length < 4){
+		alert("ERROR: please be more specific");
+		taskinput.value = "";
+	}
+
+	else if (taskinput.value.length > 60) {
+		alert ("ERROR: please be more brief");
+		taskinput.value = "";
+	}	
+		else {
+
+			// Update the DOM
+			const li = document.createElement("li");
+			ul.appendChild(li);
+			li.appendChild(document.createTextNode(taskinput.value));
+
+			// Store in LocalStorage
+			storeInLocalStorage(taskinput.value);
+
+			// Clear Task Value
+			taskinput.value = "";
+		}
+});
+
+// Event Listener for the Clear Button (Clear Tasks AND Empties Storage)
 clearbutton.addEventListener("click", function(){
 	var tasks = document.getElementById("tasked");
 	while (tasks.hasChildNodes()){
